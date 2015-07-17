608c25d8b219627f6defe903cf91b12cfa70219e
gabrideoliver <gabrieloliveira_96@hotmail.com.br>
2015-07-17 10:59:47 -0300

    Update and rename LICENSE.md to ForEncadeado.cs

---
+++ ForEncadeado.cs
@@ -0,0 +1,14 @@
+// o Código ira repetir as linas conforme o numero das colunas
+// , e seus multipos de acordo com o numero de colunas
+
+      //linha
+for (int i = 1; i <= 10; I++)
+{
+    //Coluna
+  for (int j = 1 ; j < = i ; j++)
+  {     // Imprimir Coluna e Linha , e pular uma coluna.
+      Console.Write(j*i + "\t");
+      }
+      //Puklar uma linha depois da repetição
+      Console.WriteLine("");
+} 
